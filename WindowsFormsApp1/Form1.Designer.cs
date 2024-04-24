404d9ded16ce70040e507ffcae0a4d916cdc0254
Kstatin Hack <eremeevlasha@yandex.ru>
2024-04-24 23:25:18 +0300

    project WinForm_Haffman

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DictBtn = new System.Windows.Forms.Button();
+            this.CodeBtn = new System.Windows.Forms.Button();
+            this.EncodeBtn = new System.Windows.Forms.Button();
+            this.DecodeBtn = new System.Windows.Forms.Button();
+            this.ChoseBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DictBtn
+            // 
+            this.DictBtn.BackColor = System.Drawing.Color.White;
+            this.DictBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.DictBtn.FlatAppearance.BorderSize = 10;
+            this.DictBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DictBtn.Location = new System.Drawing.Point(25, 24);
+            this.DictBtn.Name = "DictBtn";
+            this.DictBtn.Size = new System.Drawing.Size(366, 90);
+            this.DictBtn.TabIndex = 1;
+            this.DictBtn.Text = "Make a dictionary";
+            this.DictBtn.UseVisualStyleBackColor = false;
+            this.DictBtn.Click += new System.EventHandler(this.DictBtn_Click);
+            // 
+            // CodeBtn
+            // 
+            this.CodeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CodeBtn.Location = new System.Drawing.Point(25, 133);
+            this.CodeBtn.Name = "CodeBtn";
+            this.CodeBtn.Size = new System.Drawing.Size(366, 90);
+            this.CodeBtn.TabIndex = 2;
+            this.CodeBtn.Text = "Make a code";
+            this.CodeBtn.UseVisualStyleBackColor = true;
+            this.CodeBtn.Click += new System.EventHandler(this.CodeBtn_Click);
+            // 
+            // EncodeBtn
+            // 
+            this.EncodeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EncodeBtn.Location = new System.Drawing.Point(25, 244);
+            this.EncodeBtn.Name = "EncodeBtn";
+            this.EncodeBtn.Size = new System.Drawing.Size(170, 90);
+            this.EncodeBtn.TabIndex = 3;
+            this.EncodeBtn.Text = "Encode";
+            this.EncodeBtn.UseVisualStyleBackColor = true;
+            this.EncodeBtn.Click += new System.EventHandler(this.EncodeBtn_Click);
+            // 
+            // DecodeBtn
+            // 
+            this.DecodeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DecodeBtn.Location = new System.Drawing.Point(221, 244);
+            this.DecodeBtn.Name = "DecodeBtn";
+            this.DecodeBtn.Size = new System.Drawing.Size(170, 90);
+            this.DecodeBtn.TabIndex = 4;
+            this.DecodeBtn.Text = "Decode";
+            this.DecodeBtn.UseVisualStyleBackColor = true;
+            this.DecodeBtn.Click += new System.EventHandler(this.DecodeBtn_Click);
+            // 
+            // ChoseBtn
+            // 
+            this.ChoseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChoseBtn.Location = new System.Drawing.Point(25, 363);
+            this.ChoseBtn.Name = "ChoseBtn";
+            this.ChoseBtn.Size = new System.Drawing.Size(366, 90);
+            this.ChoseBtn.TabIndex = 5;
+            this.ChoseBtn.Text = "Choose the file";
+            this.ChoseBtn.UseVisualStyleBackColor = true;
+            this.ChoseBtn.Click += new System.EventHandler(this.ChoseBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(424, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(591, 520);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1041, 544);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ChoseBtn);
+            this.Controls.Add(this.DecodeBtn);
+            this.Controls.Add(this.EncodeBtn);
+            this.Controls.Add(this.CodeBtn);
+            this.Controls.Add(this.DictBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button DictBtn;
+        private System.Windows.Forms.Button CodeBtn;
+        private System.Windows.Forms.Button EncodeBtn;
+        private System.Windows.Forms.Button DecodeBtn;
+        private System.Windows.Forms.Button ChoseBtn;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string text = null;
+        StringBuilder haffCode = new StringBuilder();
+        Dictionary<char, string> codes;
+        Dictionary<char, int> dict;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dig = new OpenFileDialog();
+            if (dig.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(dig.FileName))
+                {
+                    text = sr.ReadToEnd();
+                }
+                textBox1.Text = text;
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
+        }
+
+        private void DictBtn_Click(object sender, EventArgs e)
+        {
+            if (text == null)
+            {
+                MessageBox.Show("Choose the text,please");
+            }
+            else
+            {
+                dict = new Dictionary<char, int>();
+                foreach (char c in text)
+                {
+                    if (!dict.ContainsKey(c))
+                    {
+                        dict.Add(c, 1);
+                    }
+                    else
+                    {
+                        dict[c]++;
+                    }
+                }
+                textBox1.Text = "";                
+                foreach (KeyValuePair<char, int> pair in dict)
+                {
+                    textBox1.Text += '"' + pair.Key.ToString() + '"' + " - " + pair.Value.ToString() + '\r' + '\n';
+                }
+            }
+
+        }
+
+        private void CodeBtn_Click(object sender, EventArgs e)
+        {
+            if (text == null)
+            {
+                MessageBox.Show("Choose the text,please");
+            }
+            else
+            {
+                var frequencies = new Dictionary<char, int>();
+                frequencies = dict;
+
+                var tree = new HuffmanTree(dict);
+                codes = tree.GetCodes();
+
+                textBox1.Text = "";
+                foreach (KeyValuePair<char, string> pair in codes)
+                {
+                    textBox1.Text += '"' + pair.Key.ToString() + '"' + " - " + pair.Value + '\r'+'\n';
+                }
+            }
+        }
+        public class HuffmanNode
+        {
+            public char Symbol { get; set; }
+            public int Frequency { get; set; }
+            public HuffmanNode Left { get; set; }
+            public HuffmanNode Right { get; set; }
+        }
+
+        public class HuffmanTree
+        {
+            private HuffmanNode root;
+
+            public HuffmanTree(Dictionary<char, int> frequencies)
+            {
+                var nodes = new List<HuffmanNode>();
+                foreach (var kvp in frequencies)
+                {
+                    nodes.Add(new HuffmanNode { Symbol = kvp.Key, Frequency = kvp.Value });
+                }
+
+                while (nodes.Count > 1)
+                {
+                    nodes.Sort((x, y) => x.Frequency.CompareTo(y.Frequency));
+
+                    var left = nodes[0];
+                    var right = nodes[1];
+                    var parent = new HuffmanNode { Frequency = left.Frequency + right.Frequency, Left = left, Right = right };
+
+                    nodes.Remove(left);
+                    nodes.Remove(right);
+                    nodes.Add(parent);
+                }
+
+                root = nodes[0];
+            }
+
+            public Dictionary<char, string> GetCodes()
+            {
+                var codes = new Dictionary<char, string>();
+                Transition(root, "", codes);
+                return codes;
+            }
+
+            private void Transition(HuffmanNode node, string code, Dictionary<char, string> codes)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                if (node.Symbol != '\0')
+                {
+                    codes[node.Symbol] = code;
+                }
+
+                Transition(node.Left, code + "0", codes);
+                Transition(node.Right, code + "1", codes);
+            }
+        }
+
+        private void DecodeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EncodeBtn_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("code.txt"))
+                foreach (var item in text)
+                {
+                    haffCode.Append(codes[item]);
+                    sw.Write(codes[item]);
+                }
+            DecodeBtn.BackColor = Color.Green;
+        }
+    }
+}
